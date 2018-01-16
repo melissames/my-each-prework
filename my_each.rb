34c833a92a4ff4266fd56a58880224563cce885d
Melissa Sames <melissa.sames@gmail.com>
2018-01-16 19:31:55 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 def my_each (collection)
   i = 0
+  array = collection.to_a
 
-  while i < collection.length
-    yield collection[i]
+  while i < array.length
+    yield array[i]
     i = i + 1
   end
 end
